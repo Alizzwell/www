ef9d6e8a95e0b574094c3aa6bab6b076bbac9c03
SEC <SEC>
2016-07-18 15:28:16 +0900

    thumbnail upload

--- models/algorithm.js
+++ models/algorithm.js
@@ -4,8 +4,8 @@ var Schema = mongoose.Schema;
 var algorithmSchema = new Schema({
 	category: String,
 	subject: String,
-  inputData: String,
-  code: String
+	inputData: String,
+	code: String
 });
 
 module.exports = mongoose.model('algorithm', algorithmSchema);
