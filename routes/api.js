c11b517497d7506adcd05dc5c24fedc1ed89118c
--global <tusbea@gmail.com>
2016-07-20 16:56:00 +0900

    api changed - post 분리, execute impl with async

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,24 @@
+
+var filenameIndex = 1;
+var imagenameIndex = 1;
+var tmpImagePath = 'tmp/images/';
+var imagePath = 'public/images/thumb/';
+var sourcePath = 'tmp/source_code/';
+var testjsPath = 'public/code/';
+
+
 var multer = require('multer');
 var fs = require('fs');
 var util = require('util');
 var exec = require('child_process').exec;
+var async = require('async');
 
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, 'public/images/thumb/');
+		cb(null, tmpImagePath);
 	},
 	filename: function(req, file, cb) {
-		cb(null, req.body.category + " - " + req.body.subject);
+		cb(null, imagenameIndex++ + '_' + file.originalname);
 	}
 });
 
@@ -26,26 +36,19 @@ result code
   4 : 0 or 2 more items(when update)
 */
 
-var filenameIndex = 1;
-var sourcePath = 'tmp/source code/';
-var testjsPath = 'public/code/';
 
 module.exports = function(app, Algorithm) {
-
-	
 	// algorithms
 	app.get('/api/algorithms', function(req, res) {
 		Algorithm.find({}, {"category": 1, "subject": 1, "_id": 0}, function(err, data) {
-		if (err)
-			return res.json({"result": 1});
-		else
-			res.json(data);
+			if (err)
+				return res.json({"result": 1});
+			else
+				res.json(data);
 		})
 	});
   
-  
-
-	app.post('/api/algorithms', function(req, res) {
+	app.post('/api/image_upload', function (req, res) {
 		upload(req, res, function(err) {
 			if (err) {
 				// upload file size exceeded
@@ -57,58 +60,61 @@ module.exports = function(app, Algorithm) {
 				return;
 			}
 			
-			Algorithm.find({"subject": req.body.subject}, function(err, data) {
-				if (data.length != 0) {
-					res.json({"result": 2});
-					return;
-				}
-				
-				var algorithm = new Algorithm();
-				algorithm.category = req.body.category;
-				algorithm.subject = req.body.subject;
-				algorithm.inputData = req.body.inputData;
-				algorithm.code = req.body.code;
+			res.json({image_file_name: req.file.filename});
+		});
+	});
+
+	app.post('/api/algorithms', function(req, res) {
+		Algorithm.find({"subject": req.body.subject}, function(err, data) {
+			if (data.length != 0) {
+				res.json({"result": 2});
+				return;
+			}
 			
-				algorithm.save(function(err) {
-					if (err)
-						res.json({"result": 1});
-					else
-						res.json({"result": 0});
-				});
+			var category = req.body.category;
+			var subject = req.body.subject;
+			var inputData = req.body.inputData;
+			var code = req.body.code;
+			if (category && subject && code) {
+				res.json({"result": 4});
+				return;
+			}
+			
+			var algorithm = new Algorithm();
+			algorithm.category = category;
+			algorithm.subject = subject;
+			algorithm.inputData = inputData;
+			algorithm.code = code;
+			
+			var img_fn = req.body.image_file_name;
+			fs.createReadStream(tmpImagePath + img_fn)
+				.pipe(fs.createWriteStream(imagePath + img_fn.substring(img_fn.indexOf('_') + 1)));
+		
+			algorithm.save(function(err) {
+				if (err)
+					res.json({"result": 1});
+				else
+					res.json({"result": 0});
 			});
 		});
 	});
 	
-
-	app.put('/api/algorithms/:subject', function(req, res) {
-		upload(req, res, function(err) {
-			// upload file size exceeded
-			if (err.code == "LIMIT_FILE_SIZE") {
-				res.json({"result": 3});
-				return;
-			}
-			else if (err) {
-				res.json({"result": 1});
+	app.put('/api/algorithms/:subject', function(req, res) {		
+		Algorithm.find({subject: req.params.subject}, function(err, data) {
+			if (data.length != 1) {
+				res.json({"result": 4});
 				return;
 			}
 			
-			Algorithm.find({subject: req.params.subject}, function(err, data) {
-				if (data.length != 1) {
-					res.json({"result": 4});
-					return;
-				}
-				
-				Algorithm.update({subject: req.params.subject}, {$set: req.body}, function(err, doc) {
-					if (err)
-						res.json({"result": 1});
-					else
-						res.json({"result": 0});
-				});
+			Algorithm.update({subject: req.params.subject}, {$set: req.body}, function(err, doc) {
+				if (err)
+					res.json({"result": 1});
+				else
+					res.json({"result": 0});
 			});
 		});
 	});
 	
-
 	app.delete('/api/algorithms/:subject', function(req, res) {
 		Algorithm.remove({subject: req.params.subject}, function(err) {
 			if (err)
@@ -118,93 +124,87 @@ module.exports = function(app, Algorithm) {
 		});
 	});
 	
-	
 	app.get('/api/algorithms/:subject', function(req, res) {
 		Algorithm.findOne({'subject': req.params.subject}, function(err, data) {
-		if (err)
-			res.json({"result": 1});
-		else
-			res.json(data);
+			if (err)
+				res.json({"result": 1});
+			else
+				res.json(data);
 		});
 	});
 	
 	
-
 	/*
 	코드 업로드 & 실행 & json으로 response
 	*/
 	app.post('/api/execute', function(req, res) {
-		upload(req, res, function(err) {
-			if (req.body.targets.length == 0 ||
-				req.body.code === "" ||
-				req.body.input === "") {
-				res.json({"result": 1});
-				return;
-			}
-			
-			fs.writeFile(
-				sourcePath + filenameIndex + '.cpp',
-				req.body.code,
-				function (err) {
-					if (err) {
-						res.json({"result": 1});
-						return;
-					}
+		var code = req.body.code;
+		var targets = req.body.targets;
+		var input = req.body.input;
+		var bps = req.body.bps;
+		
+		if (targets.length == 0 ||
+			code === "") {
+			res.json({"result": 1});
+			return;
+		}
+		
+		async.waterfall([
+			function (cb) {
+				fs.writeFile(
+					sourcePath + filenameIndex + '.cpp',
+					code,
+					cb
+				);
+			},
+			function (cb) {				
+				fs.writeFile(
+					sourcePath + filenameIndex + '.txt',
+					input,
+					cb
+				);
+			},
+			function (cb) {				
+				var cmd = util.format('g++ %s -o %s', 
+					sourcePath + filenameIndex + '.cpp',
+					sourcePath + filenameIndex + '.out');
 					
-					fs.writeFile(
-						sourcePath + filenameIndex + '.txt',
-						req.body.input,
-						function(err) {
-							if (err) {
-								res.json({"result": 1});
-								return;
-							}
-							
-							// g++ 실행, 에러 처리
-							var cmd = util.format('g++ %s -o %s', 
-								sourcePath + filenameIndex + '.cpp',
-								sourcePath + filenameIndex + '.out');
-							exec(cmd, function(err, stdout, stderr) {
-								if (err) {
-									res.json({"result": 1});
-									return;
-								}
-								
-								// test.js 실행
-								var cmd = util.format('node %stest.js %s %s ',
-									testjsPath,
-									sourcePath + filenameIndex + '.cpp',
-									sourcePath + filenameIndex + '.out');
-								
-								cmd += '--targets ';
-								for (var i in req.body.targets) {
-									cmd += req.body.targets[i]
-									cmd += ' ';
-								}
-								
-								if (req.body.bps.length != 0) {
-									cmd += '--breaks ';
-									for (var i in req.body.bps) {
-										cmd += req.body.bps[i];
-										cmd += ' ';
-									}
-								}
-								
-								cmd += '--input ';
-								cmd += (sourcePath + filenameIndex + '.txt');
-								
-								exec(cmd, function(err, stdout, stderr) {
-									if (err) {
-										res.json({"result": 1});
-										return;
-									}
-									
-									filenameIndex++;
-									res.json(stdout);
-								});
-							});
-						});
-				});
+				exec(cmd, cb);
+			},
+			function (stdout, stderr, cb) {				
+				var cmd = util.format('node %stest.js %s %s ',
+					testjsPath,
+					sourcePath + filenameIndex + '.cpp',
+					sourcePath + filenameIndex + '.out');
+					
+				cmd += '--targets ';
+				for (var i in targets) {
+					cmd += targets[i]
+					cmd += ' ';
+				}
+				
+				if (bps && bps.length != 0) {
+					cmd += '--breaks ';
+					for (var i in bps) {
+						cmd += bps[i];
+						cmd += ' ';
+					}
+				}
+				
+				cmd += '--input ';
+				cmd += (sourcePath + filenameIndex + '.txt');
+				
+				exec(cmd, cb);
+			},
+			function (stdout, stderr, cb) {
+				filenameIndex++;
+				res.json(stdout);
+			}
+		], function (err) {
+			if (err) {
+				console.log(err);
+				res.json({"result": 1});
+			}
 		});
 	});
 };
